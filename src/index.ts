95e3d97bff645e0496c238c500ce585e266b109b
mrorigo <mrorigo@gmail.com>
2026-03-14 12:53:11 +0100

    bugfix: add missing getPearls export to top level exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 export { loadConfig, setConfig, getConfig } from "./config";
-export { getPearls } from "./core/pearls";
 export { getQueue } from "./core/queue";
 export { Conductor } from "./services/conductor";
 export { getWorkflowEngine } from "./services/workflow-engine";
 export { getFormulaRegistry, type Formula } from "./core/formula";
 export { startBridge } from "./bridge/index";
-export { type Pearl } from "./core/pearls";
+export { type Pearl, getPearls } from "./core/pearls";
 export { logger, type LogEntry, type LogLevel } from "./core/logger";
